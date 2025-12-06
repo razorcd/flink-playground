0b094ffd10944edda0e6e1366fd849dd4fdabef9
razor <razorcd@yahoo.com>
2025-12-06 20:22:58 +0100

    flink - kafka example

---
+++ src/main/java/org/example/StreamSet7Kafka.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example;
+
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.ConfigConstants;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+//First start in terminal:  nc -lk 9001
+public class StreamSet7Kafka {
+
+	public static void main(String[] args) throws Exception {
+		final String currentPath = System.getProperty("user.dir");
+		final String inputFilename = currentPath+"/src/main/resources/inputStream3.txt";
+		final String outputFilename = currentPath+"/src/main/resources/outputStream3.txt";
+		Files.deleteIfExists(Paths.get(outputFilename));
+
+		Map<String, String> config = new HashMap<>();
+		config.put("host", "localhost");
+		config.put("port", "9001");
+
+		// Set up Flink configuration to expose the Web UI
+		Configuration flinkConfig = new Configuration();
+		flinkConfig.setBoolean(ConfigConstants.LOCAL_START_WEBSERVER, true);
+
+		// Use LocalStreamEnvironment to enable the Flink Web UI
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(flinkConfig);
+		final ParameterTool parameterTool = ParameterTool.fromArgs(args).mergeWith(ParameterTool.fromMap(config));
+		env.getConfig().setGlobalJobParameters(parameterTool);
+
+
+		// KafkaSource (new API)
+		KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
+				.setBootstrapServers("localhost:9092")
+				.setTopics("test1")
+				.setGroupId("flink-group-1")
+				.setStartingOffsets(OffsetsInitializer.earliest())
+				.setValueOnlyDeserializer(new SimpleStringSchema())
+				.build();
+
+		// Add Kafka source using new API
+		DataStreamSource<String> kafkaStream = env.fromSource(
+				kafkaSource,
+				WatermarkStrategy.noWatermarks(),
+				"Kafka Source"
+		);
+
+		kafkaStream
+				.filter(line -> line.matches("[0-9].*"))
+				.map(Integer::parseInt)
+				.setParallelism(5)
+				.keyBy(line -> line % 5)
+				.reduce((acc, x) -> acc + x)
+				.print();
+
+
+		env.execute("Listening to socket stream");
+
+	}
+}
